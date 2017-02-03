0e8d600902ac9418c9a3fe60d07686d469fea3ef
xesam <xesamguo@gmail.com>
2017-02-03 17:40:26 +0800

    NetUtils demo

--- android-kit-demo/src/main/java/dev/xesam/android/kit/demo/MainActivity.java
+++ android-kit-demo/src/main/java/dev/xesam/android/kit/demo/MainActivity.java
@@ -2,8 +2,10 @@ package dev.xesam.android.kit.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import dev.xesam.android.androidkit.demo.R;
+import dev.xesam.android.kit.util.NetUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("MainActivity", NetUtils.getNetInfo(this).toString());
     }
 }
