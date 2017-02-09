ea50d96bebe54abae3d4800b7f89ec381b7ce76d
xesam <xesamguo@gmail.com>
2017-02-09 16:46:00 +0800

    删除废弃

--- android-kit-lib/src/main/java/dev/xesam/android/kit/util/PackageUtils.java
+++ android-kit-lib/src/main/java/dev/xesam/android/kit/util/PackageUtils.java
@@ -1,11 +1,16 @@
 package dev.xesam.android.kit.util;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.text.TextUtils;
 
 import java.security.MessageDigest;
+import java.util.List;
 
 /**
  * 获取 App 包相关的信息
@@ -13,6 +18,47 @@ import java.security.MessageDigest;
  * Created by xesamguo@gmail.com on 11/17/15.
  */
 public final class PackageUtils {
+
+    public static ActivityInfo[] getAllActivityInfo(Context context) {
+        try {
+            return context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_ACTIVITIES).activities;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return new ActivityInfo[]{};
+    }
+
+    public static void getInstalled(Context context) {
+
+        List<PackageInfo> packages = context.getPackageManager().getInstalledPackages(0);
+
+        for (int i = 0; i < packages.size(); i++) {
+            PackageInfo packageInfo = packages.get(i);
+//            AppInfo tmpInfo = new AppInfo();
+//            tmpInfo.appName = packageInfo.applicationInfo.loadLabel(getPackageManager()).toString();
+//            tmpInfo.packageName = packageInfo.packageName;
+//            tmpInfo.versionName = packageInfo.versionName;
+//            tmpInfo.versionCode = packageInfo.versionCode;
+//            tmpInfo.appIcon = packageInfo.applicationInfo.loadIcon(getPackageManager());
+//            appList.add(tmpInfo);
+        }
+    }
+
+    /**
+     * check system app
+     */
+    public static boolean isSystemApp(PackageInfo packageInfo) {
+        return (packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 1;
+    }
+
+    public static void uninstall(Context context, String packageName) {
+        Uri uri = Uri.parse("package:" + packageName);
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_DELETE);
+        intent.setData(uri);
+        context.startActivity(intent);
+
+    }
 
     public static final class Version {
         String versionName = "unknown";
